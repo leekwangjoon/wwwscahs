a498d1b6422fc72a378b0250e6538811bb6e7232
leekwangjoon <sayjun@scahs.kr>
2026-07-09 18:50:54 +0900

    Update sw.js

--- couns/sw.js
+++ couns/sw.js
@@ -1,5 +1,9 @@
+
+
+
+
 // sw.js 파일 내용
-const CACHE_NAME = 'scahs-v7'; // 👈 버전을 v1에서 v2로 변경하여 강제 업데이트 유도
+const CACHE_NAME = 'scahs-v5'; // 👈 버전을 v1에서 v2로 변경하여 강제 업데이트 유도
 const ASSETS = [
   './',                  /* 상대경로 점(.) 추가 */
   './index.html',        /* 상대경로 점(.) 추가 */
@@ -34,3 +38,5 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
+
